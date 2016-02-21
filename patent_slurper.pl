7e48cbb971d65ce29216d32916bd13135b493042
Rumps <bgoldsworthy96@gmail.com>
2016-02-21 14:07:05 +0000

    Moving

--- patent_slurper.pl
+++ patent_slurper.pl
@@ -6,12 +6,25 @@ use warnings;
 use XML::Parser;
 use XML::SimpleObject;
 
-my $file = 'ipg150106.xml';
+my( $filename ) = @ARGV;
 
-my $parser = XML::Parser->new(ErrorContext => 2, Style => "Tree");
-my $xso = XML::SimpleObject->new( $parser->parsefile($file) );
+open IN, '<', $filename or die;
+my @contents = <IN>;
+close IN;
 
-foreach my $patent ($xso->child('us-patent-grant')) {
-    print $patent->child('invention-title')->{VALUE};
-    print "\n";
-}
+@contents = grep !/^\<\?xml/igm, @contents;
+
+open OUT, '>', $filename or die;
+print OUT @contents;
+close OUT;
+
+#
+#my $file = 'ipg150106.xml';
+
+#my $parser = XML::Parser->new(ErrorContext => 2, Style => "Tree");
+#my $xso = XML::SimpleObject->new( $parser->parsefile($file) );
+
+#foreach my $patent ($xso->child('us-patent-grant')) {
+    #print $patent->child('invention-title')->{VALUE};
+   # print "\n";
+#}
